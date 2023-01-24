2e8d12158cbbc25f768b6dc5774cd417d6bf6c6d
Jordi Caballol <jcaballol94@gmail.com>
2023-01-24 22:48:54 +0100

    Fix accessibility of the aspect template

--- Templates/AspectTemplate.cs
+++ Templates/AspectTemplate.cs
@@ -5,7 +5,7 @@ using Unity.Mathematics;
 using UnityEngine;
 
     #ROOTNAMESPACEBEGIN#
-readonly partial struct #SCRIPTNAME# : IAspect
+public readonly partial struct #SCRIPTNAME# : IAspect
 {
     public readonly Entity Self;
 }
