92ad7f2a9f67aa3fddc598f43b9e3a3f3ebccfb3
Jordi Caballol <jcaballol94@gmail.com>
2023-02-12 13:31:46 +0100

    Calculate the child's position from the parent's

---
+++ Assets/ECS/Systems/GridTransforms/CalculateChildPosition.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Tetris
+{
+    [BurstCompile]
+    [RequireMatchingQueriesForUpdate]
+    [UpdateInGroup(typeof(GridTransformsSystemGroup))]
+    [UpdateAfter(typeof(UpdateParentPositionSystem))]
+    [UpdateAfter(typeof(AddMissingPositionComponentSystem))]
+    public partial struct CalculateChildPositionSystem : ISystem
+    {
+        [BurstCompile]
+        [WithChangeFilter(typeof(ParentPosition), typeof(LocalPosition))]
+        public partial struct CalculateChildPositionJob : IJobEntity
+        {
+            [BurstCompile]
+            private void Execute(in ParentPosition parentPos, in LocalPosition localPos, ref Position pos)
+            {
+                pos.value = parentPos.value + localPos.value;
+            }
+        }
+
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+        }
+
+        [BurstCompile]
+        public void OnDestroy(ref SystemState state)
+        {
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            new CalculateChildPositionJob().ScheduleParallel();
+        }
+    }
+}
