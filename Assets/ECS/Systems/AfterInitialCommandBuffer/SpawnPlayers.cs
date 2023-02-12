158b890ff64a86b76dc10d2e796ae0aef5170567
Jordi Caballol <jcaballol94@gmail.com>
2023-02-12 20:01:01 +0100

    Revert "Refactor the player spawning logic into a player spawner aspect"
    This reverts commit 8cd7aad40d4c9c021d22004b19a063ace4e44173.

--- Assets/ECS/Systems/AfterInitialCommandBuffer/SpawnPlayers.cs
+++ Assets/ECS/Systems/AfterInitialCommandBuffer/SpawnPlayers.cs
@@ -22,16 +22,31 @@ namespace Tetris
         public void OnUpdate(ref SystemState state)
         {
             var ecb = new EntityCommandBuffer(Unity.Collections.Allocator.Temp);
-            foreach (var (spawner, gameModeEntity) in 
-                SystemAPI.Query<PlayerSpawner>()
+            foreach (var (playerDefinitions, tetriminos, blockPfefab, spawnPosition, gameModeEntity) in 
+                SystemAPI.Query<DynamicBuffer<PlayerDefinitionBuffer>, DynamicBuffer<AvailableTetriminoBuffer>, RefRO<BlockPrefab>, RefRO<SpawnPosition>>()
                 .WithNone<ActivePlayerBuffer>()
                 .WithEntityAccess())
             {
                 var playerBuffer = ecb.AddBuffer<ActivePlayerBuffer>(gameModeEntity);
 
-                for (int i = 0; i < spawner.PlayersToCreate; ++i)
+                foreach (var playerDef in playerDefinitions)
                 {
-                    playerBuffer.Add(new ActivePlayerBuffer { value = spawner.SpawnEntity(ref ecb, i) });
+                    var playerEntity = ecb.CreateEntity();
+                    ecb.SetName(playerEntity, "Player");
+
+                    // Create a player and give it a reference to the game mode so it can be initialized
+                    ecb.AddComponent<PlayerTag>(playerEntity);
+                    ecb.AddComponent(playerEntity, blockPfefab.ValueRO);
+                    ecb.AddComponent(playerEntity, spawnPosition.ValueRO);
+                    ecb.AddComponent(playerEntity, new GridRef { value = playerDef.grid });
+
+                    var tetriminoesBuffer = ecb.AddBuffer<AvailableTetriminoBuffer>(playerEntity);
+                    foreach (var tetrimino in tetriminos)
+                    {
+                        tetriminoesBuffer.Add(tetrimino);
+                    }
+
+                    playerBuffer.Add(new ActivePlayerBuffer { value = playerEntity });
                 }
             }
 
