058aab4041ad5aa5f368f03eb52455a11a230c38
Jordi Caballol <jcaballol94@gmail.com>
2023-02-20 19:58:22 +0100

    Add a cleanup system that automatically deletes all the events

---
+++ Assets/Scripts/Events/Systems/CleanupEvents.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Tetris
+{
+    [BurstCompile]
+    [RequireMatchingQueriesForUpdate]
+    [UpdateInGroup(typeof(EventsSystemGroup), OrderLast = true)]
+    public partial struct CleanupEventsSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+        }
+
+        [BurstCompile]
+        public void OnDestroy(ref SystemState state)
+        {
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            state.EntityManager.DestroyEntity(SystemAPI.QueryBuilder()
+                .WithAll<EventTag>()
+                .Build());
+        }
+    }
+}
