e2c2247e7fb754ed11126d32f893c28ddacb7d44
Jordi Caballol <jcaballol94@gmail.com>
2023-02-21 17:56:00 +0100

    Add burst attributes and make the code more clear in the drop system

--- Assets/Scripts/InGame/Tetrimino/Systems/TetriminoDrop.cs
+++ Assets/Scripts/InGame/Tetrimino/Systems/TetriminoDrop.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using UnityEngine;
@@ -13,16 +14,19 @@ namespace Tetris
     [UpdateAfter(typeof(TetriminoMovementSystem))]
     public partial struct TetriminoDropSystem : ISystem
     {
+        [BurstCompile]
         public void OnCreate(ref SystemState state)
         {
             state.RequireForUpdate<GameData>();
             state.RequireForUpdate<PlaceTetriminoEvent>();
         }
 
+        [BurstCompile]
         public void OnDestroy(ref SystemState state)
         {
         }
 
+        [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
             if (!SystemAPI.TryGetSingleton(out GameData gameData)) return;
@@ -37,26 +41,34 @@ namespace Tetris
                 var input = state.EntityManager.GetComponentData<InputValues>(player.ValueRO.value);
                 var collider = state.EntityManager.GetAspectRO<GridCollisions>(grid.ValueRO.value);
 
+                // Calculate how much we need to drop
                 var newDropAmount = dropState.ValueRO.currentDrop;
                 if (input.drop)
                     newDropAmount += gameData.dropLength;
                 else
                     newDropAmount += gameData.fallSpeed * deltaTime * (input.fall ? gameData.fastFallMultiplier : 1f);
 
+                // Drop 1 cell at a time
                 while (newDropAmount >= 1f)
                 {
                     newDropAmount -= 1f;
-                    if (!movement.TryMove(new int2(0, -1), collider))
+
+                    // Successful movement, continue dropping
+                    if (movement.TryMove(new int2(0, -1), collider))
+                        continue;
+
+                    var transform = movement.LocalTransform;
+                    if (dropState.ValueRO.HasMoved(transform))
+                    {
+                        // If it has moved since the last collision, we store the new position
+                        // The tetrimino stays alive (the player can slide before they are placed)
+                        dropState.ValueRW.lastCollision = transform;
+                    }
+                    else
                     {
                         // If it hasn't moved since the last collision, place it
-                        var transform = movement.LocalTransform;
-                        if (dropState.ValueRO.HasMoved(transform))
-                            dropState.ValueRW.lastCollision = transform;
-                        else
-                        {
-                            placeEvents.Add(new PlaceTetriminoEvent { tetrimino = entity });
-                            break;
-                        }
+                        placeEvents.Add(new PlaceTetriminoEvent { tetrimino = entity });
+                        break;
                     }
                 }
 
