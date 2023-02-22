f4b1bdf6ceb4a54802ab88e80a30deae1152a371
Jordi Caballol <jcaballol94@gmail.com>
2023-02-22 21:29:00 +0100

    Add system to detect lines

--- Assets/Scripts/InGame/GameMode/Systems/DetectLines.cs
+++ Assets/Scripts/InGame/GameMode/Systems/DetectLines.cs
@@ -10,14 +10,28 @@ namespace Tetris
     [BurstCompile]
     [RequireMatchingQueriesForUpdate]
     [UpdateInGroup(typeof(VariableRateSimulationSystemGroup))]
+    [UpdateAfter(typeof(UpdateGridCollisionsSystem))]
     public partial struct DetectLinesSystem : ISystem
     {
         [BurstCompile]
         public partial struct DetectLinesJob : IJobEntity
         {
             [BurstCompile]
-            private void Execute()
+            private void Execute(in GridBounds bounds, in DynamicBuffer<GridCellData> cells, ref DynamicBuffer<RemoveLineEvent> events)
             {
+                var lineSize = bounds.size.x;
+                for (int i = 0; i < cells.Length; i += lineSize)
+                {
+                    var line = true;
+                    for (int j = 0; line && j < lineSize; ++j)
+                    {
+                        line = !cells[i * lineSize + j].available;
+                    }
+                    if (line)
+                    {
+                        events.Add(new RemoveLineEvent { lineY = (i / lineSize) });
+                    }
+                }
             }
         }
 
@@ -34,6 +48,7 @@ namespace Tetris
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
+            new DetectLinesJob().ScheduleParallel();
         }
     }
 }
