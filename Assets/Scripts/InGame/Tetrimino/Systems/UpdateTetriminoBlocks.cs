a460934c6004189055e902539c5e018825ce50fd
Jordi Caballol <jcaballol94@gmail.com>
2023-02-25 21:16:37 +0100

    Properly update the tetriminos after swapping the on hold

---
+++ Assets/Scripts/InGame/Tetrimino/Systems/UpdateTetriminoBlocks.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Rendering;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Tetris
+{
+    [BurstCompile]
+    [RequireMatchingQueriesForUpdate]
+    [UpdateInGroup(typeof(VariableRateSimulationSystemGroup))]
+    public partial struct UpdateTetriminoBlocksSystem : ISystem
+    {
+        [BurstCompile]
+        [WithChangeFilter(typeof(TetriminoData))]
+        public partial struct UpdateTetriminoBlocksJob : IJobEntity
+        {
+            public ComponentLookup<BlockPosition> positionLookup;
+            public ComponentLookup<URPMaterialPropertyBaseColor> colorLookup;
+
+            [BurstCompile]
+            private void Execute(in TetriminoData data, in DynamicBuffer<TetriminoBlockBuffer> blocks)
+            {
+                for (int i = 0; i < blocks.Length; ++i)
+                {
+                    var position = positionLookup.GetRefRW(blocks[i].value, false);
+                    position.ValueRW.position = data.blocks[i];
+
+                    var color = colorLookup.GetRefRW(blocks[i].value, false);
+                    color.ValueRW.Value = data.color;
+                }
+            }
+        }
+
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+        }
+
+        [BurstCompile]
+        public void OnDestroy(ref SystemState state)
+        {
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            var positionLookup = SystemAPI.GetComponentLookup<BlockPosition>();
+            var colorLookup = SystemAPI.GetComponentLookup<URPMaterialPropertyBaseColor>();
+
+            new UpdateTetriminoBlocksJob
+            {
+                colorLookup = colorLookup,
+                positionLookup = positionLookup
+            }.Schedule();
+        }
+    }
+}
